5c0a8f6377f96b74cd27e9d4d1bf620638db3cb2
Santino Vicentini <vicen.lan2017@gmail.com>
2023-03-08 10:57:56 -0300

    ejercicio clase 2

--- clase2/chart_01_x/script.js
+++ clase2/chart_01_x/script.js
@@ -5,8 +5,18 @@ d3.csv('data2000.csv', d3.autoType).then(data => {
     marks: [
       Plot.dot(data, {
         x: 'fertility',
+        y: 'life_expect',
+        r: 'pop',
+        fill: 'cluster',
+        symbol: "cluster",
+        opacity: 0.7,
       }),
     ],
+    color: {
+      legend: true,
+    },
+    x: { grid: true, line: true, zero: true, nice: true, },
+    y: { grid: true, line: true, zero: true, nice: true, }
   })
   // Agregamos chart al div#chart de index.html
   d3.select('#chart').append(() => chart)
